ff3cd79f1081b0d0acf12e3d86530bf33bf8c4f4
dmithamo <dennisbmithamo@gmail.com>
2020-09-08 15:35:43 +0300

    chore(clean-up): clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
-import './App.css';
-import logo from './logo.svg';
 
-function App() {
+export default function Intro() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      &copy;2020&nbsp;
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/dmithamo"
+      >
+        dmithamo
+      </a>
     </div>
   );
 }
-
-export default App;
