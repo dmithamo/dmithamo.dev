fafda26fb5a14becee72778a0d82e5d8ec39dfb3
dmithamo <dennis@novek.io>
2020-09-12 10:49:50 +0300

    feat(card-style): restyle the contacts card

--- src/features/contacts-page/ContactCard.tsx
+++ src/features/contacts-page/ContactCard.tsx
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 /* eslint-disable no-undef */
 import React from 'react';
 import styled from 'styled-components';
@@ -7,14 +8,47 @@ import contactInfo from './contactInfo';
 export default function ContactCard(): JSX.Element {
   return (
     <StyledContactCard>
-      <div className="container">
-        <div className="logo">dm</div>
+      <div className="card card-front">
+        <p className="name">{contactInfo.name}</p>
+        <p className="subscript">Software Engineer</p>
+      </div>
+      <div className="card card-back">
+        <a href="mailto:dennisbmithamo@gmail.com" className="contact-link">
+          <FontAwesomeIcon icon="envelope-open" />
+          <p className="domain">&lt;a href=&quot;mailto:&quot;&gt;</p>
+          <p className="username">{contactInfo.emailAddress}</p>
+          <p className="domain">&lt;/a&gt;</p>
+        </a>
 
-        <div className="contacts">
-          <p className="fullname">{contactInfo.name}</p>
-          <p className="link">{contactInfo.emailAddress}</p>
-          <p className="link">{contactInfo.phoneNumber}</p>
-        </div>
+        <a
+          href={[contactInfo.github.url, contactInfo.github.username].join('')}
+          className="contact-link"
+        >
+          <FontAwesomeIcon icon={['fab', 'github']} />
+          <p className="domain">
+            &lt;a href=&quot;
+            {contactInfo.github.url}
+            &quot;&gt;
+          </p>
+          <p className="username">{contactInfo.github.username}</p>
+          <p className="domain">&lt;/a&gt;</p>
+        </a>
+
+        <a
+          href={[contactInfo.linkedIn.url, contactInfo.linkedIn.username].join(
+            '',
+          )}
+          className="contact-link"
+        >
+          <FontAwesomeIcon icon={['fab', 'linkedin']} />
+          <p className="domain">
+            &lt;a href=&quot;
+            {contactInfo.linkedIn.url}
+            &quot;&gt;
+          </p>
+          <p className="username">{contactInfo.linkedIn.username}</p>
+          <p className="domain">&lt;/a&gt;</p>
+        </a>
       </div>
     </StyledContactCard>
   );
@@ -27,26 +61,42 @@ const StyledContactCard = styled.div`
   align-items: center;
   width: 100%;
 
-  div.container {
+  div.card {
     box-shadow: 1px 2px 5px 1px ${colors.veryLightBlack};
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    width: 25%;
     height: 25vh;
+    width: 48%;
     margin: auto;
 
-    div.logo {
-      width: fit-content;
-      background-color: ${colors.black};
-      color: ${colors.white};
+    p.name {
       font-size: 3em;
       font-weight: 700;
-      padding: 0 0.5em;
     }
 
-    div.contact-info {
-      flex-grow: 1;
+    p.subscript {
+      opacity: 0.8;
+      font-size: 0.8em;
+      margin: 0;
+    }
+  }
+
+  div.card-front {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: ${colors.black};
+    color: ${colors.white};
+  }
+
+  div.card-back {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    background-color: ${colors.white};
+    color: ${colors.black};
+
+    div.contact-link {
     }
   }
 `;
--- src/features/contacts-page/contactInfo.ts
+++ src/features/contacts-page/contactInfo.ts
@@ -3,9 +3,9 @@ export default {
   role: 'Software Engineer',
   emailAddress: 'dennisbmithamo@gmail.com',
   phoneNumber: '+254 722 202843',
-  github: { username: 'dmithamo', url: 'https://github.com/dmithamo' },
+  github: { username: 'dmithamo', url: 'https://github.com/' },
   linkedIn: {
-    username: 'Dennis Mithamo',
-    url: 'https://www.linkedin.com/in/dennis-mithamo',
+    username: 'dennis-mithamo',
+    url: 'https://www.linkedin.com/in/',
   },
 };
